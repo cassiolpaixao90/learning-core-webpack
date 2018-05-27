aef190a6f5956ae953196b1508ca7f2a0febe393
Cássio Paixão <cassiopaixaoti@gmail.com>
2018-05-27 17:49:11 -0300

    generate principal page dinamic

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,8 +3,22 @@ const babiliPlugin = require('babili-webpack-plugin')
 const extractTextPlugin = require('extract-text-webpack-plugin');
 const optimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 let plugins = [];
+
+plugins.push(new HtmlWebpackPlugin({
+    hash: true,
+    minify: {
+        html5: true,
+        collapseWhitespace: true,
+        removeComments: true,
+    },    
+    filename: 'index.html',
+    template: __dirname + '/main.html'
+}));
+
 plugins.push(
     new extractTextPlugin("styles.css")
 );
@@ -48,7 +62,6 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        publicPath: 'dist'
     },
     module:{
         rules:[
