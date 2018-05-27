387fe1356decc5df57c4508f9661482d7cebc587
Cássio Paixão <cassiopaixaoti@gmail.com>
2018-05-27 16:07:02 -0300

    add cross-env to set NODE_ENV

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const babiliPlugin = require('babili-webpack-plugin')
 
 let plugins = [];
-if(process.env.NODE_ENV == 'productioin'){
+if(process.env.NODE_ENV == 'production'){
     plugins.push(new babiliPlugin())
 }
 
